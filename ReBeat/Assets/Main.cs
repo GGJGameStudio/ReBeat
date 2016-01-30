1370d5ced2b0a932bf83b8191b97967a126ec738
cyril.savary.42@gmail.com <fzefre@fezrf.com>
2016-01-30 01:33:00 +0100

    first commit

---
+++ ReBeat/Assets/Main.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    for(int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                Vector3 pos = new Vector3(i, j, 0) * 0.32f;
+                Instantiate(Resources.Load("blanc"), pos, Quaternion.identity);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
