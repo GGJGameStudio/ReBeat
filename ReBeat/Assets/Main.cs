adde86c690c78a5eb46a728125c9a7074e0afc92
cyril.savary.42@gmail.com <fzefre@fezrf.com>
2016-01-31 15:18:04 +0100

    operation beure salée

--- ReBeat/Assets/Main.cs
+++ ReBeat/Assets/Main.cs
@@ -102,6 +102,11 @@ public class Main : MonoBehaviour {
         {
             player = (GameObject)Instantiate(Resources.Load("Player/Prefabs/caramel_0"), playerstartpos, Quaternion.identity);
             player.transform.localScale = new Vector3(3 * (float)tilesize / resourcetilesize, 3 * (float)tilesize / resourcetilesize);
+
+            GameObject background = (GameObject)Instantiate(Resources.Load("Player/Prefabs/karamel_0"), Vector3.zero, Quaternion.identity);
+            background.transform.localScale = new Vector3(10f, 10f, 1);
+            background.GetComponent<SpriteRenderer>().sortingOrder = -1;
+
         } else
         {
             player = (GameObject)Instantiate(Resources.Load("Player/Prefabs/penrose01"), playerstartpos, Quaternion.identity);
